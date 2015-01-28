dcabcf9e3bc6753cbd392c83ac39bbebad27a63a
Tzoockee <costin.nila@hotmail.com>
2015-01-28 18:32:18 +0200

    some error handling

--- database.py
+++ database.py
@@ -1,17 +1,24 @@
 __author__ = 'Costin'
 
 import pyodbc
+import wx
 
 connString = 'DRIVER={SQL Server};SERVER=LAPTOP\SQLSERVEREXPRESS;DATABASE=NumereDB;UID=sa;PWD=alibaba'
 
 def AddNewUser(username, firstname, lastname):
-    conn = pyodbc.connect(connString, autocommit=True)
-    curs = conn.cursor()
-    curs.execute('INSERT INTO USERS(username, firstname, lastname, password) VALUES(?, ?, ?, ?)', username, firstname, lastname, username)
-    conn.close()
+    try:
+        conn = pyodbc.connect(connString, autocommit=True)
+        curs = conn.cursor()
+        curs.execute('INSERT INTO USERS(username, firstname, lastname, password) VALUES(?, ?, ?, ?)', username, firstname, lastname, username)
+        conn.close()
+    except pyodbc.Error, err:
+        wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
 
 def DeleteUser(username):
-    conn = pyodbc.connect(connString, autocommit=True)
-    curs = conn.cursor()
-    curs.execute('DELETE FROM USERS WHERE username = ''?''', username)
-    conn.close()
+    try:
+        conn = pyodbc.connect(connString, autocommit=True)
+        curs = conn.cursor()
+        curs.execute('DELETE FROM USERS WHERE username = ''?''', username)
+        conn.close()
+    except pyodbc.Error, err:
+        wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
