980d00be1f43f6eb804c391b2608e92ce748d11f
Tzoockee <costin.nila@hotmail.com>
2015-01-28 18:19:02 +0200

    Added new user

--- admin_ui.py
+++ admin_ui.py
@@ -1,79 +1,91 @@
 import wx 
 import newUser
+import pyodbc
 
 class UsersTab(wx.Panel):
-	def __init__(self, parent):
-		wx.Panel.__init__(self, parent)
-		
-		#users list control
-		self._userList = wx.ListCtrl(self, -1, style=wx.LC_REPORT | wx.LC_SINGLE_SEL)
-		self._userList.InsertColumn(1, 'Utilizator')
-		self._userList.InsertColumn(2, 'Nume')
-		self._userList.InsertColumn(3, 'Prenume')
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent)
+        
+        #users list control
+        self._userList = wx.ListCtrl(self, -1, style=wx.LC_REPORT | wx.LC_SINGLE_SEL)
+        self._userList.InsertColumn(1, 'Utilizator')
+        self._userList.InsertColumn(2, 'Nume')
+        self._userList.InsertColumn(3, 'Prenume')
   
-		#buttons
-		newBtn = wx.Button(self, wx.ID_ANY, 'Adauga')
-		deleteBtn = wx.Button(self, wx.ID_ANY, 'Sterge')				
-		btnSizer = wx.BoxSizer(wx.HORIZONTAL)
-		btnSizer.Add(newBtn, 0, wx.ALL, 5)
-		btnSizer.Add(deleteBtn, 0, wx.ALL, 5)
+        #buttons
+        newBtn = wx.Button(self, wx.ID_ANY, 'Adauga')
+        deleteBtn = wx.Button(self, wx.ID_ANY, 'Sterge')                
+        btnSizer = wx.BoxSizer(wx.HORIZONTAL)
+        btnSizer.Add(newBtn, 0, wx.ALL, 5)
+        btnSizer.Add(deleteBtn, 0, wx.ALL, 5)
   
-		#layout
-		topSizer = wx.BoxSizer(wx.VERTICAL)
-		topSizer.Add(wx.StaticText(self, wx.ID_ANY, 'Utilizatori'), 0, wx.ALL|wx.EXPAND, 5)
-		topSizer.Add(self._userList, 0, wx.ALL|wx.EXPAND, 5)
-		topSizer.Add(btnSizer, 0, wx.ALL|wx.EXPAND, 5)
-		self.SetSizer(topSizer)
-		topSizer.Fit(self)
-		
-		#events
-		self.Bind(wx.EVT_BUTTON, self.OnNewUser, newBtn)
-		self.Bind(wx.EVT_BUTTON, self.OnDeleteUser, deleteBtn)
-		
-		#initialize
-		self.RefreshUserList()
-		
-	def OnNewUser(self, event):
-		newUserDlg = newUser.NewUser(None, -1, 'Utilizator nou')
-		ret = newUserDlg.ShowModal()		
-		if ret == wx.ID_OK:
-			print newUserDlg.GetName()
-			#TODO: Create the user
-			self.RefreshUserList()
-		else:
-			print 'CANCEL'
-		newUserDlg.Destroy()
-		
-	def OnDeleteUser(self, event):
-		index = 0
-		
-	def RefreshUserList(self):
-		self._userList.DeleteAllItems()
-		
+        #layout
+        topSizer = wx.BoxSizer(wx.VERTICAL)
+        topSizer.Add(wx.StaticText(self, wx.ID_ANY, 'Utilizatori'), 0, wx.ALL|wx.EXPAND, 5)
+        topSizer.Add(self._userList, 0, wx.ALL|wx.EXPAND, 5)
+        topSizer.Add(btnSizer, 0, wx.ALL|wx.EXPAND, 5)
+        self.SetSizer(topSizer)
+        topSizer.Fit(self)
+        
+        #events
+        self.Bind(wx.EVT_BUTTON, self.OnNewUser, newBtn)
+        self.Bind(wx.EVT_BUTTON, self.OnDeleteUser, deleteBtn)
+        
+        #initialize
+        self.RefreshUserList()
+        
+    def OnNewUser(self, event):
+        newUserDlg = newUser.NewUser(None, -1, 'Utilizator nou')
+        ret = newUserDlg.ShowModal()        
+        if ret == wx.ID_OK:
+            self._userList.DeleteAllItems()
+            conn = pyodbc.connect('DRIVER={SQL Server};SERVER=LAPTOP\SQLSERVEREXPRESS;DATABASE=NumereDB;UID=sa;PWD=alibaba', autocommit=True)
+            curs = conn.cursor()
+            curs.execute('INSERT INTO USERS(username, firstname, lastname, password) VALUES(?, ?, ?, ?)', newUserDlg.GetUserName(), newUserDlg.GetFirstName(), newUserDlg.GetLastName(), newUserDlg.GetUserName())
+            conn.close()
+            self.RefreshUserList()
+        newUserDlg.Destroy()
+        
+    def OnDeleteUser(self, event):
+        index = 0
+        
+    def RefreshUserList(self):
+        self._userList.DeleteAllItems()
+        conn = pyodbc.connect('DRIVER={SQL Server};SERVER=LAPTOP\SQLSERVEREXPRESS;DATABASE=NumereDB;UID=sa;PWD=alibaba', autocommit=True)
+        curs = conn.cursor()
+        curs.execute('SELECT username, firstname, lastname FROM USERS')
+        index = 0
+        for row in curs:
+            self._userList.InsertStringItem(index, str(row.username))
+            self._userList.SetStringItem(index, 1, str(row.firstname))
+            self._userList.SetStringItem(index, 2, str(row.lastname))
+            index = index + 1
+        conn.close()
+        
 class Tabs(wx.Notebook):
-	def __init__(self, parent):
-		wx.Notebook.__init__(self, parent, id=wx.ID_ANY, style=wx.BK_DEFAULT)
-		
-		usersTab = UsersTab(self)
-		self.AddPage(usersTab, "Utilizatori")
-		
-		docsTab = wx.Panel(self)
-		self.AddPage(docsTab, "Documente")
-		
-		othersTab = wx.Panel(self)
-		self.AddPage(othersTab, "Altele")
-		
+    def __init__(self, parent):
+        wx.Notebook.__init__(self, parent, id=wx.ID_ANY, style=wx.BK_DEFAULT)
+        
+        usersTab = UsersTab(self)
+        self.AddPage(usersTab, "Utilizatori")
+        
+        docsTab = wx.Panel(self)
+        self.AddPage(docsTab, "Documente")
+        
+        othersTab = wx.Panel(self)
+        self.AddPage(othersTab, "Altele")
+        
 class MainFrame(wx.Frame):
-	def __init__(self, *args, **kwargs):
-		super(MainFrame, self).__init__(*args, **kwargs) 
-		panel = wx.Panel(self)
-		notebook = Tabs(panel)
-		sizer = wx.BoxSizer(wx.VERTICAL)
-		sizer.Add(notebook, 1, wx.ALL|wx.EXPAND, 5)
-		panel.SetSizer(sizer)
-		sizer.Fit(self)
-		self.Layout()
-		
+    def __init__(self, *args, **kwargs):
+        super(MainFrame, self).__init__(*args, **kwargs) 
+        panel = wx.Panel(self)
+        notebook = Tabs(panel)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        sizer.Add(notebook, 1, wx.ALL|wx.EXPAND, 5)
+        panel.SetSizer(sizer)
+        sizer.Fit(self)
+        self.Layout()
+        
 app = wx.App()
 frame = MainFrame(None, -1, 'Admin')
 frame.Show()
--- newUser.py
+++ newUser.py
@@ -1,55 +1,58 @@
 import wx 
 
 class NewUser(wx.Dialog):
-	def __init__(self, *args, **kwargs):
-		super(NewUser, self).__init__(*args, **kwargs) 
-		
-		# Add a panel so it looks correct on all platforms
-		self.panel = wx.Panel(self, wx.ID_ANY)
+    def __init__(self, *args, **kwargs):
+        super(NewUser, self).__init__(*args, **kwargs) 
+        
+        # Add a panel so it looks correct on all platforms
+        self.panel = wx.Panel(self, wx.ID_ANY)
  
-		labelName = wx.StaticText(self.panel, wx.ID_ANY, 'Nume')
-		self._name = wx.TextCtrl(self.panel, wx.ID_ANY, '')
+        labelName = wx.StaticText(self.panel, wx.ID_ANY, 'Nume')
+        self._lastName = wx.TextCtrl(self.panel, wx.ID_ANY, '')
  
-		labelFirstName = wx.StaticText(self.panel, wx.ID_ANY, 'Prenume')
-		self._firstName = wx.TextCtrl(self.panel, wx.ID_ANY, '')
+        labelFirstName = wx.StaticText(self.panel, wx.ID_ANY, 'Prenume')
+        self._firstName = wx.TextCtrl(self.panel, wx.ID_ANY, '')
  
-		labelUserName = wx.StaticText(self.panel, wx.ID_ANY, 'Utilizator')
-		self._userName = wx.TextCtrl(self.panel, wx.ID_ANY, '')
+        labelUserName = wx.StaticText(self.panel, wx.ID_ANY, 'Utilizator')
+        self._userName = wx.TextCtrl(self.panel, wx.ID_ANY, '')
  
-		okBtn = wx.Button(self.panel, wx.ID_OK, 'OK')		
-		cancelBtn = wx.Button(self.panel, wx.ID_CANCEL, 'Cancel')
+        okBtn = wx.Button(self.panel, wx.ID_OK, 'OK')        
+        cancelBtn = wx.Button(self.panel, wx.ID_CANCEL, 'Cancel')
  
-		topSizer		= wx.BoxSizer(wx.VERTICAL)
-		inputOneSizer   = wx.BoxSizer(wx.HORIZONTAL)
-		inputTwoSizer   = wx.BoxSizer(wx.HORIZONTAL)
-		inputThreeSizer = wx.BoxSizer(wx.HORIZONTAL)
-		btnSizer		= wx.BoxSizer(wx.HORIZONTAL)
+        topSizer        = wx.BoxSizer(wx.VERTICAL)
+        inputOneSizer   = wx.BoxSizer(wx.HORIZONTAL)
+        inputTwoSizer   = wx.BoxSizer(wx.HORIZONTAL)
+        inputThreeSizer = wx.BoxSizer(wx.HORIZONTAL)
+        btnSizer        = wx.BoxSizer(wx.HORIZONTAL)
  
-		inputOneSizer.Add(labelName, 0, wx.ALL, 5)
-		inputOneSizer.Add(self._name, 1, wx.ALL|wx.EXPAND, 5)
+        inputOneSizer.Add(labelName, 0, wx.ALL, 5)
+        inputOneSizer.Add(self._lastName, 1, wx.ALL|wx.EXPAND, 5)
  
-		inputTwoSizer.Add(labelFirstName, 0, wx.ALL, 5)
-		inputTwoSizer.Add(self._firstName, 1, wx.ALL|wx.EXPAND, 5)
+        inputTwoSizer.Add(labelFirstName, 0, wx.ALL, 5)
+        inputTwoSizer.Add(self._firstName, 1, wx.ALL|wx.EXPAND, 5)
  
-		inputThreeSizer.Add(labelUserName, 0, wx.ALL, 5)
-		inputThreeSizer.Add(self._userName, 1, wx.ALL|wx.EXPAND, 5)
+        inputThreeSizer.Add(labelUserName, 0, wx.ALL, 5)
+        inputThreeSizer.Add(self._userName, 1, wx.ALL|wx.EXPAND, 5)
  
-		btnSizer.Add(okBtn, 0, wx.ALL, 5)
-		btnSizer.Add(cancelBtn, 0, wx.ALL, 5)
+        btnSizer.Add(okBtn, 0, wx.ALL, 5)
+        btnSizer.Add(cancelBtn, 0, wx.ALL, 5)
  
-		topSizer.Add(wx.StaticLine(self.panel), 0, wx.ALL|wx.EXPAND, 5)
-		topSizer.Add(inputOneSizer, 0, wx.ALL|wx.EXPAND, 5)
-		topSizer.Add(inputTwoSizer, 0, wx.ALL|wx.EXPAND, 5)
-		topSizer.Add(inputThreeSizer, 0, wx.ALL|wx.EXPAND, 5)
-		topSizer.Add(wx.StaticLine(self.panel), 0, wx.ALL|wx.EXPAND, 5)
-		topSizer.Add(btnSizer, 0, wx.ALL|wx.CENTER, 5)
+        topSizer.Add(wx.StaticLine(self.panel), 0, wx.ALL|wx.EXPAND, 5)
+        topSizer.Add(inputOneSizer, 0, wx.ALL|wx.EXPAND, 5)
+        topSizer.Add(inputTwoSizer, 0, wx.ALL|wx.EXPAND, 5)
+        topSizer.Add(inputThreeSizer, 0, wx.ALL|wx.EXPAND, 5)
+        topSizer.Add(wx.StaticLine(self.panel), 0, wx.ALL|wx.EXPAND, 5)
+        topSizer.Add(btnSizer, 0, wx.ALL|wx.CENTER, 5)
  
-		self.panel.SetSizer(topSizer)
-		topSizer.Fit(self)
-		
-		
-	def GetName(self):
-		return self._firstName.GetValue() + ' ' + self._name.GetValue()
-	
-	def GetUserName(self):
-		return self._username.GetValue()
+        self.panel.SetSizer(topSizer)
+        topSizer.Fit(self)
+        
+
+    def GetUserName(self):
+        return self._userName.GetValue()
+
+    def GetFirstName(self):
+        return self._firstName.GetValue()
+
+    def GetLastName(self):
+        return self._lastName.GetValue()
