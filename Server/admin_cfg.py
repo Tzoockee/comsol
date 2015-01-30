a20a85ef2b8a37a3405888a17b8571239d46f5f3
unknown <mso1m8@BRA-W12-008.lmsintl.com>
2015-01-30 10:19:13 +0200

    config file

---
+++ Server/admin_cfg.py
@@ -0,0 +1,3 @@
+#Accesul la baza de date
+#DB_Connection = 'DRIVER={SQL Server};SERVER=LAPTOP\SQLSERVEREXPRESS;DATABASE=NumereDB;UID=sa;PWD=alibaba'
+DB_Connection = 'DSN=NumereDB;Trusted_Connection=yes'
