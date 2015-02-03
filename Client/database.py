5bc4ae0284f41f8dfe4b880bc84131fc94501c5d
Tzoockee <costin.nila@hotmail.com>
2015-02-03 10:49:03 +0200

    Created helper functions

--- Client/database.py
+++ Client/database.py
@@ -11,72 +11,58 @@ def Connection():
     curs.execute('SET DATEFORMAT dmy')
     return conn
 
-def TestLogin(username, password):
+def SelectRows(query, *args):
     try:
         conn = Connection()
         curs = conn.cursor()
-        curs.execute('SELECT COUNT(*) AS Status FROM USERS WHERE username = ? and password = ?', username, password)
-        row = curs.fetchone()
-        conn.close()
-        status = False if row.Status < 1 else True
-    except pyodbc.Error, err:
-        wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
-        return False
-    return status
-
-def ChangePassword(username, password):
-    userId = GetUserId(username)
-    try:
-        conn = Connection()
-        curs = conn.cursor()
-        curs.execute('UPDATE Users SET password = ? WHERE ID = ?', password, userId)
-        conn.close()
-    except pyodbc.Error, err:
-        wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
-
-def GetUserFullName(username):
-    try:
-        conn = Connection()
-        curs = conn.cursor()
-        curs.execute('SELECT firstname, lastname FROM USERS WHERE username = ?', username)
-        row = curs.fetchone()
-        conn.close()
-    except pyodbc.Error, err:
-        wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
-    return row.firstname + ' ' + row.lastname
-
-def GetDocumentTypes():
-    try:
-        conn = Connection()
-        curs = conn.cursor()
-        curs.execute('SELECT docType FROM DocType')
+        curs.execute(query, args)
         rows = curs.fetchall()
         conn.close()
     except pyodbc.Error, err:
         wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
+        return None
     return rows
 
-def GetUserId(username):
+def ExecuteQuery(query, *args):
     try:
         conn = Connection()
         curs = conn.cursor()
-        curs.execute('SELECT id FROM Users WHERE username = ?', username)
-        row = curs.fetchone()
+        curs.execute(query, args)
         conn.close()
     except pyodbc.Error, err:
         wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
-    return row.id
+
+def TestLogin(username, password):
+    rows = SelectRows('SELECT COUNT(*) AS Status FROM USERS WHERE username = ? and password = ?', username, password)
+    if rows is None or len(rows) == 0:
+        return False
+
+    return False if rows[0].Status < 1 else True
+
+def ChangePassword(username, password):
+    userId = GetUserId(username)
+    ExecuteQuery('UPDATE Users SET password = ? WHERE ID = ?', password, userId)
+
+def GetUserFullName(username):
+    rows = SelectRows('SELECT firstname, lastname FROM USERS WHERE username = ?', username)
+    if rows is None or len(rows) == 0:
+        return ''
+    return rows[0].firstname + ' ' + rows[0].lastname
+
+def GetDocumentTypes():
+    return SelectRows('SELECT docType FROM DocType ORDER BY docType ASC')
+
+def GetUserId(username):
+    rows = SelectRows('SELECT id FROM Users WHERE username = ?', username)
+    if rows is None or len(rows) == 0:
+        return ''
+    return rows[0].id
 
 def GetDocTypeId(docType):
-    try:
-        conn = Connection()
-        curs = conn.cursor()
-        curs.execute('SELECT id FROM DocType WHERE docType = ?', docType)
-        row = curs.fetchone()
-        conn.close()
-    except pyodbc.Error, err:
-        wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
-    return row.id
+    rows = SelectRows('SELECT id FROM DocType WHERE docType = ?', docType)
+    if rows is None or len(rows) == 0:
+        return ''
+    return rows[0].id
 
 def AddDocument(authUser, docType, userDate, lastName, firstName, filePath, description):
     userId = GetUserId(authUser)
@@ -96,23 +82,16 @@ def AddDocument(authUser, docType, userDate, lastName, firstName, filePath, desc
     return str(row.newNumber) + ' - ' + userDate
 
 def GetDocument(number):
-    try:
-        conn = Connection()
-        curs = conn.cursor()
-        curs.execute('SELECT file_path FROM Documents WHERE ID = ?', number)
-        row = curs.fetchone()
-        conn.close()
-    except pyodbc.Error, err:
-        wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
-    return row.file_path
+    rows = SelectRows('SELECT file_path FROM Documents WHERE ID = ?', number)
+    if rows is None or len(rows) == 0:
+        return ''
+    return rows[0].file_path
 
 def GetReport(authUser, docType, dateFrom, dateTo):
     userId = GetUserId(authUser)
     docTypeId = GetDocTypeId(docType)
-    try:        
-        conn = Connection()
-        curs = conn.cursor()
-        curs.execute("""SELECT 
+
+    return SelectRows("""SELECT 
                             D.id As Numar,
                             D.user_date As Data,
                             DT.docType As Tip,                            
@@ -120,9 +99,7 @@ def GetReport(authUser, docType, dateFrom, dateTo):
                         FROM 
                             Documents D INNER JOIN DocType DT ON D.doctype_id = DT.ID
                         WHERE
-                            d.docType_id = ? AND D.user_id = ? AND D.user_date >= ? AND d.user_date <= ?""", docTypeId, userId, dateFrom, dateTo)
-        rows = curs.fetchall()
-        conn.close()
-    except pyodbc.Error, err:
-        wx.MessageBox(str(err), 'Error', wx.OK | wx.ICON_ERROR)
-    return rows
+                            d.docType_id = ? AND D.user_id = ? AND D.user_date >= ? AND d.user_date <= ?
+                        ORDER BY
+                            D.id ASC""", docTypeId, userId, dateFrom, dateTo)
+
