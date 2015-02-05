6a9bc53ec08e9a5b4ee4270fccd959d7d8c5f3ed
Tzoockee <costin.nila@hotmail.com>
2015-02-05 14:37:46 +0200

    added config file

--- Client/client_cfg.py
+++ Client/client_cfg.py
@@ -1,6 +1,16 @@
-#Salvare fisiere:
-docRepositoryPath = 'C:\\Temp\\'
+docRepositoryPath = ''
+DB_Connection = ''
 
-#Accesul la baza de date
-#DB_Connection = 'DRIVER={SQL Server};SERVER=LAPTOP\SQLSERVEREXPRESS;DATABASE=NumereDB;UID=sa;PWD=alibaba'
-DB_Connection = 'DSN=NumereDB;Trusted_Connection=yes'
+if docRepositoryPath == '' and DB_Connection == '':
+    with open('client.cfg') as f:
+        content = f.readlines()
+
+    for line in content:
+        if len(line) > 0 and line[0] != '#':
+            values = line.split(' = ')
+            if len(values) == 2:
+                if values[0] == 'docRepositoryPath':
+                    docRepositoryPath = values[1].rstrip()
+                if values[0] == 'DB_Connection':
+                    DB_Connection = values[1].rstrip()
+
